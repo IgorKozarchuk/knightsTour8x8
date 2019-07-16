f80f2d7b2683d742f6d9f476b9de3610527d9b89
IgorKozarchuk <igor.a.kozarchuk@gmail.com>
2019-07-16 18:13:51 +0300

    refactor and fixes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// global vars
+// *** Global vars
 const result = document.getElementById("result");
 const knight = document.getElementById("knight");
 const resetBtn = document.getElementById("resetBtn");
@@ -6,31 +6,37 @@ let chessBoard;
 const N = 8; // board size
 const Nsqr = N * N;
 const coordLetters = "abcdefgh";
-let cellsArr = [];
+let cellSize; // size of the board cell
 let tourMoves = [];
-// flag to to avoid multiple instances of tour
-let isTourRunning = false;
-let timerId; // timer for setTimeout (moves animation)
-const speed = 300; // animation speed, ms
+let isTourRunning = false; // flag to avoid multiple instances of tour
+let timerId; // timer for setInterval (moves animation)
+const SPEED = 300; // animation speed, ms
 
-// event listeners
+// *** Event listeners
 window.onload = function() {
 	drawBoard();
-	getCellsCoords(chessBoard);
-}
+};
+// fix knight position on resize
+window.onresize = function() {
+	if (!isTourRunning && tourMoves.length > 0) {
+		let cells = chessBoard.querySelectorAll("TD");
+		let lastCell = cells[tourMoves.indexOf(Nsqr)];
 
+		knight.style.left = getCoords(lastCell).left - knight.clientWidth / 2 + "px";
+		knight.style.top = getCoords(lastCell).top - knight.clientHeight / 2 + "px";
+	}
+};
+// reset knight
 resetBtn.addEventListener("click", resetKnight);
 
-// draw chess board (main interface function)
+// *** Interface functions
+// draw chess board (main interface)
 function drawBoard() {
-	// if board already exists redraw it
 	chessBoard = document.getElementById("chessBoard");
-	if (chessBoard) chessBoard.remove();
-	tourMoves = []; // reset moves
 	// create chess board
 	chessBoard = document.createElement("TABLE");
 	chessBoard.id = "chessBoard";
-
+	// create rows and td
 	for (let i = 0; i < N; i++) {
 		let row = document.createElement("TR");
 
@@ -66,6 +72,7 @@ function drawBoard() {
 
 	setBoardSize(chessBoard, N);
 	result.appendChild(chessBoard);
+	// place knight on click and calculate tour
 	chessBoard.addEventListener("click", boardClickHandler);
 
 	function boardClickHandler(event) {
@@ -77,7 +84,6 @@ function drawBoard() {
 				isTourRunning = true;
 				placeKnight(target);
 				tourMoves = knightsTour(N, x0, y0);
-				writeMoves();
 				showMoves(chessBoard);
 				return;
 			}
@@ -85,13 +91,12 @@ function drawBoard() {
 		}
 	}
 }
-
 // set board size and knight width
 function setBoardSize(board, size) {
 	let windowWidth = document.documentElement.clientWidth;
-	let cellSize = windowWidth * 0.85 / size;
+	cellSize = windowWidth * 0.85 / size;
 	// limit max size of the cell
-	cellSize = (cellSize > 100) ? 100 : cellSize;
+	if (cellSize > 100) cellSize = 100;
 	// set size of the span inside the cell
 	for (let i = 0; i < board.rows.length; i++) {
 		for (let j = 0; j < board.rows[i].cells.length; j++) {
@@ -102,7 +107,6 @@ function setBoardSize(board, size) {
 	// set size of the knight
 	knight.style.width = cellSize * 0.8 + "px";
 }
-
 // place knight on click
 function placeKnight(cell) {
 	// get cell coords
@@ -113,106 +117,67 @@ function placeKnight(cell) {
 	knight.style.left = getCoords(cell).left - knight.clientWidth / 2 + "px";
 	knight.style.top = getCoords(cell).top - knight.clientHeight / 2 + "px";
 }
-
 // reset knight's position
 function resetKnight() {
+	clearInterval(timerId);
 	isTourRunning = false; // reset tour flag
-	clearTimeout(timerId); // stop unfinished animation
 	knight.style.display = "none";
-	knight.style.transition = "none"; // to avoid jumping into from previous position
-	tourMoves = []; // reset moves
-	// reset cells text
+	// reset left and top to avoid jumping into from previous position
+	knight.style.left = knight.style.top = "";
+	minDegIndexes = [];
+	// reset cells
 	let cellSpans = chessBoard.querySelectorAll("SPAN:not(.letterCoord):not(.numberCoord)");
 	for (let i = 0; i < cellSpans.length; i++) {
 		cellSpans[i].innerHTML = "";
 	}
 }
-
-// order of moves and coords of every cell
-function Cell(x, y, n) {
-	this.x = x; // x coordinate of the cell
-	this.y = y; // y coordinate
-	this.n = n; // cell position in knight's tour
-}
-
-// write moves
-function writeMoves() {
-	for (let i = 0; i < Nsqr; i++) {
-		cellsArr[i].n = tourMoves[i];
-	}
-}
-
-// get cells coords
-function getCellsCoords(board) {
-	if (!board) return;
-	let coords = [];
-	// get all cell spans except the ones with coords
-	let cellSpans = board.querySelectorAll("SPAN:not(.letterCoord):not(.numberCoord)");
-	
-	for (let i = 0; i < cellSpans.length; i++) {
-		coords.push({
-				x: getCoords(cellSpans[i]).left,
-				y: getCoords(cellSpans[i]).top,
-			}
-		);
-		cellsArr.push(new Cell(coords[i].x, coords[i].y));
-	}
-
-	return coords;
-}
-
 // show moves in cells
 function showMoves(board) {
 	// get all cell spans except the ones with coords
 	let cellSpans = board.querySelectorAll("SPAN:not(.letterCoord):not(.numberCoord)");
-	let knightXOffset = knight.clientWidth / 2;
-	let knightYOffset = knight.clientHeight / 2;
-	// set transition
-	knight.style.transition = "all " + speed + "ms ease";
+	// set transition for smooth animation
+	knight.style.transition = "all " + SPEED + "ms ease";
 	
-	let counter = 0;
-	
+	let counter = 0;	
 	timerId = setInterval(function() {
-		if (counter < Nsqr) {
-			// show moves starting from 2nd position
-			let objCurrent = cellsArr.find(o => o.n === counter+1);
-			let objNext = cellsArr.find(o => o.n === counter+2);
-			if (!objNext) return;
-			// show moves starting from 2nd position
-			knight.style.left = objNext.x - knightXOffset + "px";
-			knight.style.top = objNext.y - knightYOffset + "px";
-			// write move number
-			cellSpans[cellsArr.indexOf(objCurrent)].innerHTML = objCurrent.n;
+		if (counter < Nsqr-1) {
+			// show moves
+			knight.style.left = parseFloat(knight.style.left) + cellSize * dx[minDegIndexes[counter]] + "px";
+			knight.style.top = parseFloat(knight.style.top) + cellSize * dy[minDegIndexes[counter]] + "px";
+			// write move number (except the last one)
+			cellSpans[tourMoves.indexOf(counter+1)].innerHTML = counter + 1;
 			counter++;
 		} else {
 			clearInterval(timerId);
 			return;
 		}
-	}, speed);
-}
+	}, SPEED);
 
-// get coordinates in document of element's center 
+	isTourRunning = false;
+}
+// get coordinates of element's center in document
 function getCoords(elem) {
 	let box = elem.getBoundingClientRect();
 
 	return {
-		top: parseInt(box.top + pageYOffset + elem.clientWidth / 2),
-		left: parseInt(box.left + pageXOffset + elem.clientHeight / 2)
+		top: parseFloat(box.top + pageYOffset + elem.clientWidth / 2),
+		left: parseFloat(box.left + pageXOffset + elem.clientHeight / 2)
 	};
 }
 
-/*** Algorithm for Knight's tour problem using Warnsdorff's rule ***/
-/* https://www.geeksforgeeks.org/warnsdorffs-algorithm-knights-tour-problem */
+// *** Algorithm for Knight's tour problem using Warnsdorff's rule
+// https://www.geeksforgeeks.org/warnsdorffs-algorithm-knights-tour-problem
 /* We can start from any initial position of the knight on the board.
-We always move to adjacent unvisited square with minimal degree (minimum number of unvisited adjacent) */
-let x0, y0; // initial coordinates
-let x, y; // current coords of the knight
+We always move to adjacent unvisited square with minimal degree (minimum number of unvisited adjacent squares) */
+let x0, y0; // initial coordinates of the knight
+let x, y; // current coords
 // possible 8 moves (coords difference along X and Y axes)
 const dx = [1, 1, 2, 2, -1, -1, -2, -2];
 const dy = [2, -2, 1, -1, 2, -2, 1, -1];
 const D = dx.length;
-
-// Knight's tour (x0, y0 - initial position)
+let start = 0; // starting point for selecting cell with minimum degree
+let minDegIndexes = []; // indexes in dx, dy arrays
+// Knight's tour
 function knightsTour(N=8, x0=0, y0=0) {
 	// filling up the chessboard matrix with -1
 	// -1 - not visited, 1...N*N - visited
@@ -220,27 +185,28 @@ function knightsTour(N=8, x0=0, y0=0) {
 	for (let i = 0; i < Nsqr; i++) {
 		board[i] = -1;
 	}
-	// current coords are same as initial position
+	// current coords before 1st move are the same as initial position
 	x = x0;
 	y = y0;
-	// mark first move ([y*N + x] represents board as 1D array)
+	// mark first move ([y*N+x] represents board as 1D array)
 	board[y*N+x] = 1;
 	// keep picking next move using Warnsdorff's heuristic
 	for (let i = 0; i < Nsqr-1; i++) {
 		nextMove(board);
 	}
-	
+	// retry tour if it's incomplete
+	if (board.includes(-1)) {
+		knightsTour(N, x0, y0);
+	}
+
 	return board;
 }
-
 // pick next move using Warnsdorff's heuristic
 function nextMove(board) {
 	let minDegIndex = -1; // index of a move with min degree
 	let minDeg = D + 1; // minimum degree
 	let deg; // current degree
 	let nx, ny; // new x and y coords after a move
-	// random starting point (0...7) for selecting cell with minimum degree
-	let start = Math.floor(Math.random() * D);
 	// find the adjacent cell with minimum degree
 	for (let count = 0; count < D; count++) {
 		/* index in arrays of possible moves
@@ -257,6 +223,7 @@ function nextMove(board) {
 	}
 	// if next position not found
 	if (minDegIndex === -1) return false;
+	minDegIndexes.push(minDegIndex);
 	// store coords of next position
 	nx = x + dx[minDegIndex];
 	ny = y + dy[minDegIndex];
@@ -265,20 +232,19 @@ function nextMove(board) {
 	// update next position
 	x = nx;
 	y = ny;
+	// reset to avoid big numbers
+	if (++start > 255) start = 0;
 
 	return true;
 }
-
 // check whether a cell is valid and not visited
 function isEmpty(board, x, y) {
 	return (limits(x, y) && (board[y*N+x] < 0));
 }
-
 // restrict the knight to remain within the chessboard
 function limits(x, y) {
 	return (x >= 0 && y >= 0) && (x < N && y < N);
 }
-
 // number of empty cells adjacent to (x, y)
 function getDegree(board, x, y) {
 	let degree = 0;
